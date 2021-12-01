29f3db81cc5e06e87d1a50ac621fd3a8d08acbcd
pranshucoding <88497179+pranshucoding@users.noreply.github.com>
2021-12-01 16:02:03 +0400

    Add files via upload

---
+++ project c-61.js
@@ -0,0 +1,5 @@
+function cc(){ document.getElementById("color").style.color="blue";}
+
+function cc1(){ document.getElementById("color1").style.color="yellow";}
+
+function cc2(){ document.getElementById("color2").style.color="red";}
